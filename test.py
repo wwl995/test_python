0f3a8effecd493fabf0a3c3b4efc1247e9703847
wwl995 <57948674+wwl995@users.noreply.github.com>
2023-10-16 20:07:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -16,11 +16,10 @@ def process_form():
     p = request.form['p']
     if u == "wwl" and p == "wwl":
         # 打印输出数据
-        print('c:', c)
-        print('u:', u)
         print('p:', p)
         # 返回一个响应给用户，告诉他们数据已经处理完毕
-        return subprocess.check_output(c, shell=True)
+        res = subprocess.check_output(c, shell=True)
+        return res
 
 if __name__ == '__main__':
     app.run(debug=True)
