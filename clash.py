5a5dc949c29b3a480a8033dc267e5525fe0079fd
wwl995 <57948674+wwl995@users.noreply.github.com>
2023-11-09 21:16:46 +0800

    Update clash.py

--- clash.py
+++ clash.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def process_form():
+    passw = request.form['p']
+    if paswd != 'wwl':
+        return json.dumps({'code':1, 'mes':'恭喜你获得奖励！'})
     email = cl.generate_random_email()
     password = cl.generate_random_password()
     ip = cl.generate_random_ip()
@@ -15,9 +18,9 @@ def process_form():
     if authorization:
         url = cl.getSubscribe(authorization)
         print("订阅地址yaml文件：", url)
-        return json.dumps({code:1, 'url': url, 'time': time, 'authorization': authorization})
+        return json.dumps({'code':1, 'url': url, 'time': time, 'authorization': authorization, 'mes': '获取成功！'})
     else:
-        return json.dumps({code:2, msg:'出现错误，请稍后再试！'})
+        return json.dumps({'code':2, 'mes':'出现错误，请稍后再试！'})
 
 if __name__ == '__main__':
     app.run(debug=True)
